24e6669da6b3adb4ba9091a4699dde597f5e876a
lfernandosantos <lf.fernandodossantos@MacBook-Fernando.local>
2019-04-03 01:08:21 -0300

    Init pod project. Create Suggestions view model.

---
+++ Chuck Facts/SuggestionsCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  SuggestionsCollectionViewCell.swift
+//  Chuck Facts
+//
+//  Created by Luiz Fernando dos Santos on 03/04/19.
+//  Copyright © 2019 LFSantos. All rights reserved.
+//
+
+import UIKit
+
+class SuggestionsCollectionViewCell: UICollectionViewCell {
+    
+}
