735658ffff87aa4328c2548bb475b026ae44a08a
lfernandosantos <lf.fernandodossantos@MacBook-Fernando.local>
2019-04-10 22:47:46 -0300

    Created ChuckFacts View Model.

---
+++ Chuck Facts/Facts/viewModel/ChuckFactsViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  ChuckFactsViewModel.swift
+//  Chuck Facts
+//
+//  Created by Luiz Fernando dos Santos on 10/04/19.
+//  Copyright © 2019 LFSantos. All rights reserved.
+//
+
+import Foundation
+import Bond
+
+protocol ChuckFactsDelegate {
+    var factsViewModel: ChuckFactsViewModel {get set}
+    func clickShareFact(_ at: Int)
+}
+
+
+struct FactViewModel {
+    let title: String
+    let link: String
+    let category: String
+
+    init(fact: Fact) {
+        self.title = fact.value ?? " "
+        self.link = fact.url ?? " "
+        self.category = fact.category?.first?.uppercased() ?? Constants.Fact.UNCATEGORIZED.rawValue
+    }
+}
+
+
+class ChuckFactsViewModel {
+    var facts = Observable<[FactViewModel]>([])
+
+    init(facts: [FactViewModel]?) {
+        self.facts.value = facts ?? []
+    }
+}
