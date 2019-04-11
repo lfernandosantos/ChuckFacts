d50686ea0fca7f56718820be58cb98083b890fb8
lfernandosantos <lf.fernandodossantos@MacBook-Fernando.local>
2019-04-10 23:02:13 -0300

    --amend

---
+++ Chuck Facts/Search/viewModel/SearchViewModel.swift
@@ -0,0 +1,46 @@
+//
+//  SearchViewModel.swift
+//  Chuck Facts
+//
+//  Created by Luiz Fernando dos Santos on 06/04/19.
+//  Copyright © 2019 LFSantos. All rights reserved.
+//
+
+import Foundation
+import Bond
+
+class SearchViewModel {
+
+    var facts = Observable<[FactViewModel]>([])
+
+    func searchWith(suggestions: String) {
+        SearchFactsService().fetch(category: suggestions) { (result) in
+            switch result{
+            case .failure(let error):
+                print(error)
+            case .success(let fact):
+                if let fact = fact {
+                    self.facts.value = [FactViewModel(fact: fact)]
+                }
+            }
+        }
+    }
+
+
+    func searchFacts(query: String) {
+        SearchFactsService().fetch(query: query) { (result) in
+            switch result{
+            case .failure(let error):
+                print(error)
+            case .success(let searchFact):
+                if searchFact.count > 0 {
+                     DataManager.saveLastSearch(query)
+                    let listViewModel = searchFact.map({ (item) -> FactViewModel in
+                        return FactViewModel(fact: item)
+                    })
+                    self.facts.value = listViewModel
+                }
+            }
+        }
+    }
+}
---
+++ Chuck Facts/Search/viewModel/SuggestionsViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  SuggestionsViewModel.swift
+//  Chuck Facts
+//
+//  Created by Luiz Fernando dos Santos on 03/04/19.
+//  Copyright © 2019 LFSantos. All rights reserved.
+//
+
+import Foundation
+import Bond
+
+class SuggestionsViewModel {
+    
+    var listSuggestions = Observable<[String]>([])
+
+    init() {
+        listSuggestions.value = DataManager.loadSuggestions()
+        if listSuggestions.value.isEmpty {
+            fetchSuggestions()
+        }
+    }
+
+
+    func fetchSuggestions() {
+        CategoriesService().fetch { (result) in
+            switch result{
+            case .failure(let error):
+                print(error)
+            case .success(let list):
+                DataManager.saveSugestions(list)
+                self.listSuggestions.value = DataManager.loadSuggestions()
+            }
+        }
+    }
+}
