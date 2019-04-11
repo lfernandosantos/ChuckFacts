3fac27a7ad638a118ca482608970723dd8c880ce
lfernandosantos <lf.fernandodossantos@MacBook-Fernando.local>
2019-04-10 22:40:03 -0300

    Created Localizable string file.

---
+++ Chuck Facts/StringFile.swift
@@ -0,0 +1,14 @@
+//
+//  StringFile.swift
+//  Chuck Facts
+//
+//  Created by Luiz Fernando dos Santos on 10/04/19.
+//  Copyright © 2019 LFSantos. All rights reserved.
+//
+
+import Foundation
+
+enum StringFile: String {
+    case emptyList =  "list.empty"
+}
+
