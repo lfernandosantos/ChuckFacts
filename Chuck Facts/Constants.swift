bda65f206a48ef41f6ea96c30531307aa4b24184
lfernandosantos <lf.fernandodossantos@MacBook-Fernando.local>
2019-04-10 23:03:21 -0300

    Created Constants file.

---
+++ Chuck Facts/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Chuck Facts
+//
+//  Created by Luiz Fernando dos Santos on 09/04/19.
+//  Copyright © 2019 LFSantos. All rights reserved.
+//
+
+import Foundation
+
+enum Constants {
+    enum Fact: String {
+        case UNCATEGORIZED = "UNCATEGORIZED"
+    }
+}
